9a4c0f710fa130faf32c1c9d3c9e8d58efbc3f83
theartofnowt <michaelgordon.brown1@gmail.com>
2017-07-17 16:04:44 +0100

    back to cat

--- app.rb
+++ app.rb
@@ -1,8 +1,12 @@
 require 'sinatra'
 
 
-
-get '/graham' do
-  @name = "grumpy graham"
+get '/random-cat' do
+  @name = ["Bobby McTat", "Grumpy Graham", "San Miguel"].sample
   erb(:index)
 end
+
+# get '/named-cat' do
+#   @name = "Bobby McTat"
+#   erb(:index)
+# end
