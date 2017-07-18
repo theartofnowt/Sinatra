a0af37fbf2485da9f892d1fec19f5d6a032f724e
theartofnowt <michaelgordon.brown1@gmail.com>
2017-07-18 14:40:13 +0100

    went a bit crazy with css

--- app.rb
+++ app.rb
@@ -1,12 +1,24 @@
 require 'sinatra'
 
+get'/' do
+  'hello'
+end
+
+get '/secret' do
+  'This is a secret page, go away!'
+end
 
 get '/random-cat' do
   @name = ["Bobby McTat", "Grumpy Graham", "San Miguel"].sample
-  erb(:index)
+  erb :index
 end
 
-# get '/named-cat' do
-#   @name = "Bobby McTat"
-#   erb(:index)
-# end
+post '/named-cat' do
+  p params
+  @name = params[:name]
+  erb :index
+end
+
+get '/cat-form' do
+  erb :cat_form
+end
